f4d4c145766ea06ffec3003137dafb2019f6346f
Emily Behlmann <emilybehlmann@emilys-air.attlocal.net>
2016-12-03 19:04:09 -0600

    ugly but it works

--- 2016/day3/day3.php
+++ 2016/day3/day3.php
@@ -2,8 +2,6 @@
 
 	include 'input.php';
 
-  	$input_exploded = (explode("  ", $input));
-
 	function triangle_checker($sides) {
 		sort($sides);
 		if($sides[0] + $sides[1] <= $sides[2]) {
@@ -13,24 +11,14 @@
 		}
 	}
 
+	// for puzzle 1 only
 	function loop_through_sides($sides_array) {
 		$triangle_count = 0;
 
 		while(count($sides_array) > 0) {
-			$side_1 = intval(array_pop($sides_array));
-			if($side_1 == 0 || $side_1 == null) {
-				$side_1 = intval(array_pop($sides_array));
-			}
-
-			$side_2 = intval(array_pop($sides_array));
-			if($side_2 == 0 || $side_2 == null) {
-				$side_2 = intval(array_pop($sides_array));
-			}
-
-			$side_3 = intval(array_pop($sides_array));
-			if($side_3 == 0 || $side_3 == null) {
-				$side_3 = intval(array_pop($sides_array));
-			}
+			$side_1 = array_pop($sides_array);
+			$side_2 = array_pop($sides_array);
+			$side_3 = array_pop($sides_array);
 
 			$triangle_to_check = array($side_1, $side_2, $side_3);
 			if(triangle_checker($triangle_to_check) == true) {
@@ -40,7 +28,53 @@
 
 		return $triangle_count;
 	}
-	
-	echo loop_through_sides($input_exploded);
+
+	// for puzzle 2 only
+	function loop_through_triangles($triangle_array) {
+		$triangle_count = 0;
+
+		foreach($triangle_array as $triangle) {
+			if(triangle_checker($triangle) == true) {
+				$triangle_count++;
+			}
+		}
+
+		return $triangle_count;
+	}
+
+  	$input_exploded = (explode("  ", $input));
+
+  	$zero_indexes = array();
+
+  	$i = 0;
+
+  	while($i < count($input_exploded)) {
+  		$input_exploded[$i] = intval($input_exploded[$i]);
+  		if($input_exploded[$i] == 0) {
+  			array_push($zero_indexes, $i);
+  		}
+  		$i++;
+  	}
+
+  	foreach($zero_indexes as $index) {
+  		unset($input_exploded[$index]);
+  	}
+
+  	$input_in_nines = array_chunk($input_exploded, 9);
+  	
+  	$input_in_threes = array();
+
+  	while(count($input_in_nines) > 0) {
+  		$array_1 = array();
+  		$array_2 = array();
+  		$array_3 = array();
+  		$current_element = array_pop($input_in_nines);
+  		array_push($array_1, $current_element[0], $current_element[3], $current_element[6]);
+  		array_push($array_2, $current_element[1], $current_element[4], $current_element[7]);
+  		array_push($array_3, $current_element[2], $current_element[5], $current_element[8]);
+  		array_push($input_in_threes, $array_1, $array_2, $array_3);
+  	}
+
+  	echo loop_through_triangles($input_in_threes);
 
 ?>
