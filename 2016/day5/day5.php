1ec54533e06da3e2fd4d02800affdb428d088ab8
Emily Behlmann <emilybehlmann@emilys-air.attlocal.net>
2016-12-05 21:25:42 -0600

    day 5 complete

--- 2016/day5/day5.php
+++ 2016/day5/day5.php
@@ -1,22 +1,30 @@
 <?php
 
+	set_time_limit(180);
+
 	$input = "wtnhxymk";
 
 	$password = "";
 
 	$i = 0;
 
-	while(strlen($password) < 8) {
+	$password_array = (array("", "", "", "", "", "", "", ""));
+
+	while(in_array("", $password_array, true)) {
 		$string_to_hash = $input . $i;
 		$hash = md5($string_to_hash);
 
 		if(strcmp(substr($hash, 0, 5), "00000") == 0) {
-			$password = $password . substr($hash, 5, 1);
+			$password_index = substr($hash, 5, 1);
+			if($password_index < 8 && $password_array[$password_index] == "") {
+				$password_array[$password_index] = substr($hash, 6, 1);
+			}
 		}
 
 		$i++;
 	}
 
-	echo $password;
+	//print_r($password_array);
+	echo substr(implode("", $password_array), 0, 8);
 
 ?>
